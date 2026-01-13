cb37694a100670a758929df637f502a6a4067602
Linell Bonnette <tlbonnette@gmail.com>
2026-01-13 09:05:54 -0800

    chore: bump Inngest TS version

--- ts-express/src/fns.ts
+++ ts-express/src/fns.ts
@@ -1,12 +1,18 @@
 import { inngest } from "./client";
 
+async function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 export const fn1 = inngest.createFunction(
   {
-    id: "fn-1",
-    retries: 0,
+    id: "fn-1"
   },
   { event: "event-1" },
   async () => {
-    return "Hello world!";
+    await sleep(60 * 100);
+    return "Hello, World!";
   }
 );
