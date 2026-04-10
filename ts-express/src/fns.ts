8cea8d1e0b48aa9767ff3a64a2b89c7b697e27cf
Linell Bonnette <tlbonnette@gmail.com>
2026-04-10 14:53:54 -0700

    repro: EXE-1593

--- ts-express/src/fns.ts
+++ ts-express/src/fns.ts
@@ -6,13 +6,68 @@ async function sleep(ms: number): Promise<void> {
   });
 }
 
+// export const fn1 = inngest.createFunction(
+//   {
+//     id: "fn-1",
+//   },
+//   { event: "event-1" },
+//   async ({ step }) => {
+//     const resultOne = await step.run("step-one", async () => {
+//       await sleep(Math.random() * 1000);
+//       return 1;
+//     });
+//
+//     await sleep(60 * 100);
+//
+//     const waitForCancelOrInstantSendResult = await Promise.race([
+//       step.waitForEvent("wait-for-cancel", {
+//         event: "cancel",
+//         timeout: "60s"
+//       }),
+//       step.waitForEvent("wait-for-instant-send", {
+//         event: "send-instant",
+//         timeout: "60s"
+//       }),
+//     ]);
+//
+//     return waitForCancelOrInstantSendResult?.name;
+//   }
+// );
+
 export const fn1 = inngest.createFunction(
   {
-    id: "fn-1"
+    id: "fn-1",
+    triggers: { event: "event-1" },
   },
-  { event: "event-1" },
-  async () => {
+  async ({ step, group }) => {
+    const resultOne = await step.run("step-one", async () => {
+      await sleep(Math.random() * 1000);
+      return 1;
+    });
+
     await sleep(60 * 100);
-    return "Hello, World!";
+
+    const waitForCancelOrInstantSendResult = await group.parallel(async () => {
+      return Promise.race([
+        // step.run("slow", async () => {
+        //   await sleep(6000);
+        //   return "slow";
+        // }),
+        // step.run("fast", async () => {
+        //   await sleep(600);
+        //   return "fast";
+        // }),
+        step.waitForEvent("wait-for-cancel", {
+          event: "cancel",
+          timeout: "60s"
+        }),
+        step.waitForEvent("wait-for-instant-send", {
+          event: "send-instant",
+          timeout: "60s"
+        }),
+      ]);
+    })
+
+    return waitForCancelOrInstantSendResult?.name;
   }
 );
