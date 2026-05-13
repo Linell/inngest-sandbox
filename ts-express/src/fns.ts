b70a510dbe78eebed726772b47c18ef8b4ec844b
Linell Bonnette <tlbonnette@gmail.com>
2026-05-13 12:39:51 -0700

    chore: add defer to example

--- ts-express/src/fns.ts
+++ ts-express/src/fns.ts
@@ -9,6 +9,22 @@ export const fooDefer = createDefer(
   inngest,
   { id: "foo-defer" },
   async ({ step, event }) => {
+    sleep(3000);
+    const result = await step.run("hard-work", () => {
+      sleep(Math.random() * 1000);
+      return event.data?.foo || "Bar!";
+    })
+
+    return result;
+  }
+)
+
+export const fooInvoke = inngest.createFunction(
+  {
+    id: "foo-invoke",
+    retries: 0,
+  },
+  async ({ step, event }) => {
     const result = await step.run("hard-work", () => {
       return event.data?.foo || "Bar!";
     })
@@ -23,14 +39,18 @@ export const fn1 = inngest.createFunction(
     retries: 0,
     triggers: { event: "event-1" },
   },
-  async ({ defer }) => {
+  async ({ defer, step }) => {
     defer("foo", { function: fooDefer, data: {} }); // TODO: is sending no data useful ever?
     await sleep(500);
 
     defer("foo-two", { function: fooDefer, data: { foo: "Bazzzzzzz" } });
     await sleep(600);
 
+    const invoked = await step.invoke("foo-invoke", {
+      function: fooInvoke,
+      data: { foo: "Invoked!" },
+    });
 
-    return "Hello world!";
+    return invoked;
   },
 );
