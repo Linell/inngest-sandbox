4d859c17006eac871bd298bdead7453f63fc1dd0
Linell Bonnette <tlbonnette@gmail.com>
2026-05-14 09:14:39 -0700

    why is this no longer a repro?

--- ts-express/src/fns.ts
+++ ts-express/src/fns.ts
@@ -1,16 +1,16 @@
 import { inngest } from "./client";
 
 export const fn1 = inngest.createFunction(
-	{
-		id: "fn-1",
-		triggers: { event: "event-1" },
-	},
-	async ({ step }) => {
-		for (let i = 0; i < 10; i++) {
-			await step.run(`step-${i}`, async () => {
-				console.log(`step-${i} started`);
-				await new Promise((resolve) => setTimeout(resolve, 10_000));
-			});
-		}
-	},
+  {
+    id: "fn-1",
+    triggers: { event: "event-1" },
+  },
+  async ({ step }) => {
+    for (let i = 0; i < 10; i++) {
+      await step.run(`step-${i}`, async () => {
+        console.log(`step-${i} started`);
+        await new Promise((resolve) => setTimeout(resolve, 10_000));
+      });
+    }
+  },
 );
