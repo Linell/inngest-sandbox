b95e175ffde26179c438a29cc2c878042dd7c116
Linell Bonnette <tlbonnette@gmail.com>
2026-06-01 08:38:04 -0700

    feat(defers): add more examples

--- ts-express/src/fns.ts
+++ ts-express/src/fns.ts
@@ -7,13 +7,27 @@ const sleep = (ms: number): Promise<void> => {
 
 export const fooDefer = createDefer(
   inngest,
-  { id: "foo-defer" },
+  { id: "foo-defer", name: "Enterprise Grade Deferred Function w/Long AF Name" },
   async ({ step, event }) => {
-    sleep(3000);
-    const result = await step.run("hard-work", () => {
-      sleep(Math.random() * 1000);
+    await sleep(5000);
+    const result = await step.run("hard-work", async () => {
+      await sleep(Math.random() * 5_000);
       return event.data?.foo || "Bar!";
     })
+
+    console.log("the result is ", result)
+
+    if (result === "Bar!") {
+      console.log("Sending teh invocation!")
+      const invoked = await step.invoke("foo-invoke", {
+        function: fooInvoke,
+        data: { foo: "Invoked by Bar!!" },
+      });
+    } else if (result === "Bazzzzzzz") {
+      console.log("well....")
+    } else {
+      console.log("Fuck?")
+    }
 
     return result;
   }
@@ -22,10 +36,12 @@ export const fooDefer = createDefer(
 export const fooInvoke = inngest.createFunction(
   {
     id: "foo-invoke",
+    name: "Enterprise Friendly Invocation",
     retries: 0,
   },
   async ({ step, event }) => {
-    const result = await step.run("hard-work", () => {
+    const result = await step.run("hard-work", async () => {
+      await sleep(Math.random() * 8_000);
       return event.data?.foo || "Bar!";
     })
 
@@ -35,21 +51,22 @@ export const fooInvoke = inngest.createFunction(
 
 export const fn1 = inngest.createFunction(
   {
-    id: "fn-1",
+    id: "fn-1_ENTERPRISE_SYNC_EVENT",
+    name: "Enterprise Sync Event", // TODO: does this name show in old UI?
     retries: 0,
     triggers: { event: "event-1" },
   },
   async ({ defer, step }) => {
     defer("foo", { function: fooDefer, data: {} }); // TODO: is sending no data useful ever?
-    await sleep(500);
-
     defer("foo-two", { function: fooDefer, data: { foo: "Bazzzzzzz" } });
-    await sleep(600);
+    await sleep(5000);
 
     const invoked = await step.invoke("foo-invoke", {
       function: fooInvoke,
       data: { foo: "Invoked!" },
     });
+
+    defer("devils-reject", { function: fooDefer, data: [] });
 
     return invoked;
   },
